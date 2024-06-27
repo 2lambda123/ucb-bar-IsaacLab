e29bb7f602a59ecfc9485f82ed87eb3cc6fcae84
-T.K.- <t_k_233@outlook.com>
2024-06-27 13:22:49 -0700

    ADD: add cyberdog2 code

---
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/cyberdog2/flat_env_cfg.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from omni.isaac.lab.utils import configclass
+
+from .rough_env_cfg import Cyberdog2RoughEnvCfg
+
+
+@configclass
+class Cyberdog2FlatEnvCfg(Cyberdog2RoughEnvCfg):
+    def __post_init__(self):
+        # post init of parent
+        super().__post_init__()
+
+        # override rewards
+        self.rewards.flat_orientation_l2.weight = -2.5
+        self.rewards.feet_air_time.weight = 0.25
+
+        # change terrain to flat
+        self.scene.terrain.terrain_type = "plane"
+        self.scene.terrain.terrain_generator = None
+        # no height scan
+        self.scene.height_scanner = None
+        self.observations.policy.height_scan = None
+        # no terrain curriculum
+        self.curriculum.terrain_levels = None
+
